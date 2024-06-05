e2084937f82b214826c49bbb05c3a427ecfb6c48
Anupta <anupta22@gmail.com>
2024-06-05 16:41:19 -0400

    Added problem 200 solution -- DFS

---
+++ Number of Islands/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    private void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) return;
+        if (grid[x][y] == '0') return;
+
+        grid[x][y] = '0';
+        
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+    }
+    public int numIslands(char[][] grid) {
+        int num = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return num;
+    }
+}
