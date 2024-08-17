acd831cf51ce433f9d269d28a4ab3772d871d49f
Anupta <anupta22@gmail.com>
2024-08-16 20:27:34 -0400

    Updated problem 74 solution

--- Search a 2D Matrix/Solution.java
+++ Search a 2D Matrix/Solution.java
@@ -1,26 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rowLo = 0;
-        int rowHi = matrix.length - 1;
-        int rowMid = (rowLo + rowHi) / 2;
-
-        int colLo = 0;
-        int colHi = matrix[0].length - 1;
-        int colMid = (colLo + colHi) / 2;
-
-        while (rowLo <= rowHi) {
-            if (matrix[rowMid][colLo] <= target && target <= matrix[rowMid][colHi]) {
-                while (colLo <= colHi) {
-                    if (matrix[rowMid][colMid] == target) return true;
-                    else if (matrix[rowMid][colMid] < target) colLo = colMid + 1;
-                    else colHi = colMid - 1;
-                    colMid = (colLo + colHi) / 2;
-                }
-                return false;
-            }
-            else if (matrix[rowMid][colHi] < target) rowLo = rowMid + 1;
-            else rowHi = rowMid - 1;
-            rowMid = (rowLo + rowHi) / 2;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int lo = 0, hi = m * n - 1, mid = (lo + hi) / 2;
+        while (lo <= hi) {
+            int val = matrix[mid / n][mid % n];
+            if (val == target) return true;
+            else if (val > target) hi = mid - 1;
+            else lo = mid + 1;
+            mid = (lo + hi) / 2;
         }
         return false;
     }
